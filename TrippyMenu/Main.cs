9a8c05ab3f057f3118b6dd54c3b06b1c5c0dbfd5
Alternity156 <killercode_alternity_@hotmail.com>
2020-05-19 01:36:14 -0300

    forgot version number

--- TrippyMenu/Main.cs
+++ TrippyMenu/Main.cs
@@ -12,7 +12,7 @@ namespace TrippyMenu
         public const string Name = "TrippyMenu"; // Name of the Mod.  (MUST BE SET)
         public const string Author = "Alternity"; // Author of the Mod.  (Set as null if none)
         public const string Company = null; // Company that made the Mod.  (Set as null if none)
-        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+        public const string Version = "1.0.1"; // Version of the Mod.  (MUST BE SET)
         public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
     }
 
