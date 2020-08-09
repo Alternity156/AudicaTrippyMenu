6d4153c185fe5af05f0be74dc6eb20bdb5a59f17
Alternity156 <killercode_alternity_@hotmail.com>
2020-08-09 16:16:40 -0300

    Fixed version number

--- src/Main.cs
+++ src/Main.cs
@@ -26,7 +26,7 @@ namespace AudicaModding
             public const string Name = "TrippyMenus";  // Name of the Mod.  (MUST BE SET)
             public const string Author = "Alternity"; // Author of the Mod.  (Set as null if none)
             public const string Company = null; // Company that made the Mod.  (Set as null if none)
-            public const string Version = "0.2.1"; // Version of the Mod.  (MUST BE SET)
+            public const string Version = "1.1.2"; // Version of the Mod.  (MUST BE SET)
             public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
         }
         
